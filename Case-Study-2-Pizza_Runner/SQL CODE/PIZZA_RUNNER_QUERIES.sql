10f2c68e0d26d7e1c41e552940e1f5776d0d30c4
Jonatan Głobiński <108817948+jonatanglo@users.noreply.github.com>
2025-09-03 12:15:02 +0200

    Add files via upload

---
+++ Case-Study-2-Pizza_Runner/SQL CODE/PIZZA_RUNNER_QUERIES.sql
@@ -0,0 +1,88 @@
+USE DATABASE PIZZA_RUNNER;
+USE SCHEMA PIZZA_RUNNER;
+SELECT
+    *
+FROM CUSTOMER_ORDERS;
+
+SELECT * FROM runner_orders;
+
+--1.How many pizzas were ordered?
+SELECT 
+COUNT(*) AS ordered_pizzas
+FROM CUSTOMER_ORDERS;
+
+--2.How many unique customer orders were made?
+SELECT  COUNT(DISTINCT order_id) FROM CUSTOMER_ORDERS;
+
+--3.How many successful orders were delivered by each runner?
+SELECT 
+    runner_id,
+    COUNT(*) AS orders
+FROM runner_orders
+WHERE pickup_time IS NOT null
+GROUP BY runner_id
+ORDER BY runner_id;
+
+--4.How many of each type of pizza was delivered?
+SELECT * FROM PIZZA_NAMES;
+
+SELECT * FROM CUSTOMER_ORDERS;
+
+SELECT * FROM RUNNER_ORDERS;
+
+SELECT 
+    pizza_name,
+    COUNT (*) AS amount
+FROM CUSTOMER_ORDERS
+LEFT JOIN PIZZA_NAMES
+    ON CUSTOMER_ORDERS.pizza_id = PIZZA_NAMES.pizza_id
+LEFT JOIN RUNNER_ORDERS
+    ON CUSTOMER_ORDERS.order_id = RUNNER_ORDERS.order_id
+WHERE pickup_time IS NOT NULL
+GROUP BY pizza_name;
+
+--5.How many Vegetarian and Meatlovers were ordered by each customer?
+
+SELECT * FROM CUSTOMER_ORDERS;
+
+SELECT 
+customer_id,
+pizza_id,
+count(pizza_id)
+from customer_orders
+group by customer_id, pizza_id
+order by customer_id, pizza_id; 
+
+--6.What was the maximum number of pizzas delivered in a single order?
+SELECT *FROM CUSTOMER_ORDERS;
+
+SELECT
+    order_id,
+    COUNT(*) AS number_of_pizzas
+FROM CUSTOMER_ORDERS
+GROUP BY order_id
+ORDER BY number_of_pizzas DESC
+LIMIT 1;
+
+--7.For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+SELECT * FROM CUSTOMER_ORDERS;
+
+WITH AT_LEAST_1_CHANGE AS (
+SELECT
+*
+FROM CUSTOMER_ORDERS
+WHERE EXCLUSIONS IS NOT NULL OR EXTRAS IS NOT NULL
+)
+
+
+SELECT 
+CUSTOMER_ORDERS.customer_id,
+COUNT(AT_LEAST_1_CHANGE.*) AS at_least_1_change,
+
+FROM CUSTOMER_ORDERS
+LEFT JOIN AT_LEAST_1_CHANGE
+    ON CUSTOMER_ORDERS.customer_id = AT_LEAST_1_CHANGE.customer_id
+
+group by CUSTOMER_ORDERS.customer_id;
+
